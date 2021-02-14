615b46489e9c7b2e7389b5b2219eb73ada2b0f39
E. Hannah <echannah631@gmail.com>
2021-02-13 23:41:51 -0800

    implemented question struct, changed background colors of right and wrong answer (bug)

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -16,31 +16,56 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     
     let questions = [
-        "Kobe Bryant wore number 23 his rookie year in the NBA.",
-        "Barack Obama was the 44th President of the United States.",
-        "Netflix was founded by Warren Buffett."
+        Question(q: "A slug's blood is green.", a: "True"),
+        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+        Question(q: "Google was originally called 'Backrub'.", a: "True"),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+        
     ]
     
     var questionPosition = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         nextQuestion()
     }
     
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        questionPosition += 1
-        if (questionPosition < questions.count){
-            nextQuestion()
+        let userAnswer = sender.currentTitle!
+        let questionAnswer = questions[questionPosition].answer
+        
+        if (userAnswer == questionAnswer){
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
         }
         
+        if (questionPosition + 1 < questions.count){
+            questionPosition += 1
+        } else if questionPosition == questions.count - 1{
+            questionPosition = 0
+        }
+        
+        nextQuestion()
     }
     
     func nextQuestion(){
-        questionLabel.text = questions[questionPosition]
+        questionLabel.text = questions[questionPosition].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
     }
+    
+    
     
 
 }
